394cf638670c3f9597a8e333b4447206b24a1738
khadijaDev <khadijaoukhlift@gmail.com>
2025-07-15 17:07:28 +0200

    Nouveau projet MentorMatch

---
+++ src/test/java/com/mentormatch/MentorMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentormatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
