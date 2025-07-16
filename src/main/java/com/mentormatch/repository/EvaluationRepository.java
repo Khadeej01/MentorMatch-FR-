917c74859479f21919bf94b8047af50018f9815e
khadijaDev <khadijaoukhlift@gmail.com>
2025-07-16 10:33:14 +0200

    Evaluation controller

--- src/main/java/com/mentormatch/repository/EvaluationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mentormatch.repository;
-
-import com.mentormatch.model.Evaluation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
-}
